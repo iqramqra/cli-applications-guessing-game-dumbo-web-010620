612895e9db18902b53bd4e30f720b6ed9a7e7045
Iqra Masroor <masrooriqra@gmail.com>
2019-12-21 22:28:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -19,11 +19,4 @@ def run_guessing_game
   if input !=num
     puts "Sorry! The computer guessed #{num}."
   end
-  # if input =="exit"
-  #   puts "Goodbye!"
-  # elsif input !=num
-  #   puts "Sorry! The computer guessed #{num}."
-  # elsif input.to_i == num
-  #   puts "You guessed the correct number!"
-  # end
 end
